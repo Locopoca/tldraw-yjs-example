eb2d40ad9a6f4bcb29d01bab63660c4ff16a32ae
Ozzy <w3bbcast@gmail.com>
2024-05-24 14:08:48 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,37 @@
 import { Tldraw, track, useEditor } from 'tldraw'
 import 'tldraw/tldraw.css'
 import { useYjsStore } from './useYjsStore'
+import '@rainbow-me/rainbowkit/styles.css'
+import './index.css'
+import useCheckNftOwnership from './hooks/useCheckNftOwnership'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import {
+	arbitrum,
+	base,
+	mainnet,
+	optimism,
+	polygon,
+	sepolia,
+} from 'wagmi/chains'
+import { ConnectButton, getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+
+const config = getDefaultConfig({
+	appName: 'thePolacy DRAW',
+	projectId: 'YOUR_PROJECT_ID',
+	chains: [
+		mainnet,
+		polygon,
+		optimism,
+		arbitrum,
+		base,
+		...(process.env.VITE_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+	],
+	ssr: true,
+})
+
+const client = new QueryClient()
+let CONTRACT_ADDRESS = '0x99903e8eC87b9987bD6289DF8eff178d6E533561'
 
 const HOST_URL =
 	import.meta.env.MODE === 'development'
@@ -8,21 +39,36 @@ const HOST_URL =
 		: 'wss://demos.yjs.dev'
 
 export default function YjsExample() {
+
+	const hasNFT = useCheckNftOwnership(CONTRACT_ADDRESS)
 	const store = useYjsStore({
 		roomId: 'example17',
 		hostUrl: HOST_URL,
 	})
 
+	console.log('Rendering YjsExample: hasNFT =', hasNFT);
+
 	return (
-		<div className="tldraw__editor">
-			<Tldraw
-				autoFocus
-				store={store}
-				components={{
-					SharePanel: NameEditor,
-				}}
-			/>
-		</div>
+		<WagmiProvider config={config}>
+			<QueryClientProvider client={client}>
+				<RainbowKitProvider>
+					<div className='app-container'>
+					<ConnectButton />
+					{hasNFT ? (
+						<div className="tldraw__editor">
+							<Tldraw
+								autoFocus
+								store={store}
+								components={{
+									SharePanel: NameEditor,
+								}}
+							/>
+						</div>
+					):""}
+					</div>
+				</RainbowKitProvider>
+			</QueryClientProvider>
+		</WagmiProvider>
 	)
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,44 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import '@rainbow-me/rainbowkit/styles.css'
+import useCheckNftOwnership from './hooks/useCheckNftOwnership'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import {
+	arbitrum,
+	base,
+	mainnet,
+	optimism,
+	polygon,
+	sepolia,
+} from 'wagmi/chains'
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+
+const config = getDefaultConfig({
+	appName: 'thePolacy DRAW',
+	projectId: 'YOUR_PROJECT_ID',
+	chains: [
+		mainnet,
+		polygon,
+		optimism,
+		arbitrum,
+		base,
+		...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+	],
+	ssr: true,
+})
+
+const client = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
+	
 	<React.StrictMode>
-		<App />
+		<WagmiProvider config={config}>
+			
+							<App />
+			
+		</WagmiProvider>
 	</React.StrictMode>,
 )
